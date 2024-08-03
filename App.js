9f02705c625d4f018615ea9aa9e34bb5453db5a5
Sajib Saha <sajibsaha859@gmail.com>
2024-08-03 15:15:52 +0530

    on press header tabs

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { View, Text } from "react-native";
-
+import Home from "./screens/Home";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderTabs from "./components/HeaderTabs";
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
   );
 }
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import {
+  useSafeAreaInsets,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
