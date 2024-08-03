686caf318033ca7f85a6dc07dcfcdc069b0bb122
Sajib Saha <sajibsaha859@gmail.com>
2024-08-04 00:37:45 +0530

    Adding Search Bar

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { View, Text } from "react-native";
 import Home from "./screens/Home";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HeaderTabs from "./components/HeaderTabs";
+import SearchBar from "./components/SearchBar";
+import Categories from "./components/Categories";
 export default function App() {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories />
     </SafeAreaView>
   );
 }
--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -23,25 +23,23 @@ export default function HeaderTabs() {
   );
 }
 const HeaderButton = (props) => (
-  <View>
-    <TouchableOpacity
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.activeTab == props.text ? "black" : "white",
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text
       style={{
-        backgroundColor: props.activeTab == props.text ? "black" : "white",
-        paddingVertical: 6,
-        paddingHorizontal: 16,
-        borderRadius: 30,
+        color: props.activeTab == props.text ? "white" : "black",
+        fontSize: 15,
+        fontWeight: "900",
       }}
-      onPress={() => props.setActiveTab(props.text)}
     >
-      <Text
-        style={{
-          color: props.activeTab == props.text ? "white" : "black",
-          fontSize: 15,
-          fontWeight: "900",
-        }}
-      >
-        {props.text}
-      </Text>
-    </TouchableOpacity>
-  </View>
+      {props.text}
+    </Text>
+  </TouchableOpacity>
 );
