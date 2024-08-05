7a6b6297655636c6a6dfe616da80c12f2c90ad08
Sajib Saha <sajibsaha859@gmail.com>
2024-08-05 11:46:06 +0530

    adding assests

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
-import { SafeAreaView } from "react-native-safe-area-context";
-import HeaderTabs from "./components/HeaderTabs";
-import SearchBar from "./components/SearchBar";
-import Categories from "./components/Categories";
 export default function App() {
-  return (
-    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-      <View style={{ backgroundColor: "white", padding: 15 }}>
-        <HeaderTabs />
-        <SearchBar />
-      </View>
-      <Categories />
-    </SafeAreaView>
-  );
+  return <Home />;
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
-import {
-  useSafeAreaInsets,
-  SafeAreaProvider,
-} from "react-native-safe-area-context";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories />
+    </SafeAreaView>
   );
 }
